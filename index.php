02d676f6df398175a98516193a26e972837c1ff1
pascalc <pascal.chevrel@free.fr>
2012-12-06 18:20:27 +0100

    The library is functional for most properties files, let's call it a 1.0

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ require __DIR__ . '/helpers.php';
 
 $source = new \xformat\Properties('test.properties');
 
-echo array_to_table($source->extractProperties());
+echo array_to_table($source->analyseSource());
 dump($source->analyseSource());
 
--- properties.class.php
+++ properties.class.php
@@ -5,7 +5,7 @@
  * Licence: MPL 2/GPL 2.0/LGPL 2.1
  * Author: Pascal Chevrel, Mozilla <pascal@mozilla.com>, Mozilla
  * Date : 2012-12-05
- * version: 0.1
+ * version: 1.0
  * Description:
  * Class to extract key/value pairs from a java/js style .properties file
  * @returns array
@@ -41,57 +41,137 @@ class Properties
     public function analyseSource()
     {
         $analysis = array();
+
+        /* First pass, we categorize each line */
         foreach ($this->parsed_source as $line_nb => $line) {
 
             // Line comments
             if (substr($line[0], 0, 1) == '#') {
-                $analysis[$line_nb] = 'comment';
+                $analysis[$line_nb] = array('comment', trim(substr($line, 1)));
                 continue;
             }
 
             // Property name, check for escaped equal sign
             if (substr_count($line, '=') > substr_count($line, '\=')) {
-                $analysis[$line_nb] = 'propertyName';
+
+                $temp = explode('=', $line, 2);
+                $temp = array_map(
+                    function($elm){
+                        return trim($elm);
+                    }, $temp);
+
+                if (count($temp) == 2) {
+                    if (substr($temp[1], -1) == '"' && substr($temp[1], 0, 1) == '"') {
+                        $temp[1] = substr($temp[1], 1, -1);
+                    }
+                    $analysis[$line_nb] = array($temp[0], $temp[1]);
+                }
+                unset($temp);
                 continue;
             }
 
             // Multiline data
             if (substr_count($line, '=') == 0) {
-                $analysis[$line_nb] = 'multilineData';
+                $analysis[$line_nb] = array('multiline', $line);
                 continue;
             }
+        }
+
+
+        /* Second pass, we associate comments to entities */
+
+        // count # of comments
+        $comment_count = 0;
+        foreach ($analysis as $v) {
+            if ($v[0] == 'comment') $comment_count++;
+        }
+
+        while($comment_count > 0) {
+
+            foreach ($analysis as $line_nb => $line) {
+
+                if ($line[0] == 'comment' && isset($analysis[$line_nb+1][0]) && $analysis[$line_nb+1][0] == 'comment') {
+                    $analysis[$line_nb][1] .= ' ' . $analysis[$line_nb+1][1];
+                    $analysis[$line_nb+1][0] = 'erase';
+                    break;
+                } elseif ($line[0] == 'comment' && isset($analysis[$line_nb+1][0]) && $analysis[$line_nb+1][0] != 'multiline') {
+                    $analysis[$line_nb+1][2] = $line[1];
+                    $analysis[$line_nb][0] = 'erase';
+                }
+            }
+
+            $comment_count = 0;
+
+            foreach ($analysis as $k => $v) {
+                if ($v[0] == 'comment') $comment_count++;
+                if ($v[0] == 'erase') unset($analysis[$k]);
+            }
+
+            $analysis = array_values($analysis);
+        }
+
+        /* Third pass, we merge multiline strings */
+
+        // We remove the backslashes at end of strings if they exist
+        foreach ($analysis as $line_nb => $line) {
+            if (substr($line[1], -1) == '\\') {
+                $analysis[$line_nb][1] = trim(substr($line[1], 0, -1));
+            }
+        }
+
+        // count # of multilines
+        $counter = 0;
+        foreach ($analysis as $v) {
+            if ($v[0] == 'multiline') $counter++;
+        }
+
+        while($counter > 0) {
+
+            foreach ($analysis as $line_nb => $line) {
+
+                if ($line[0] == 'multiline'
+                    && isset($analysis[$line_nb-1][0])
+                    && $analysis[$line_nb-1][0] != 'multiline'
+                    && $analysis[$line_nb-1][0] != 'comment')
+                {
+                    $analysis[$line_nb-1][1] .= ' ' . trim($line[1]);
+                    $analysis[$line_nb][0] = 'erase';
+                    break;
+                }
+            }
+
+            $counter = 0;
+
+            foreach ($analysis as $k => $v) {
+                if ($v[0] == 'multiline') $counter++;
+                if ($v[0] == 'erase') unset($analysis[$k]);
+            }
+
+            $analysis = array_values($analysis);
+        }
+
+        /* Step 4, we clean up strings from escaped characters in properties */
+        foreach ($analysis as $k => $v) {
+            $analysis[$k][1] = str_replace('\=', '=', $v[1]);
         }
 
         return $analysis;
     }
 
-    /*
-     * This method is a quick and dirty parser that does a one-pass analyse
-     * of the properties file.
-     * Its logic is being moved to specialized methods that will allow a
-     * multipass analysis of properties file so as to handle multiline properties,
-     * comments and other edge cases
-     */
-    public function extractProperties()
-    {
-        // We parse the $parsed_source array, remove white space and delimiting quotes, skip comments
-        foreach ($this->parsed_source as $value) {
-            if (substr($value[1], 0, 1) == '#') continue;
+    public function rebuildSource() {
+        $source = $source->analyseSource();
 
-            $temp = explode('=', $value, 2);
-            $temp = array_map(
-                function($elm){
-                    return trim($elm);
-                }, $temp);
+        $comments = array();
 
-            if (count($temp) == 2) {
-                if (substr($temp[1], -1) == '"' && substr($temp[1], 0, 1) == '"') {
-                    $temp[1] = substr($temp[1], 1, -1);
-                }
-                $finalArray[$temp[0]] = $temp[1];
+        foreach ($source as $k => $v) {
+            if ($v == 'comment') {
+                $comments[] = $k;
+                continue;
+            }
+
+            if ($v == 'propertyName') {
+
             }
         }
-
-        return $finalArray;
     }
 }
