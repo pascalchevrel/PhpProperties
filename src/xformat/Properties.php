baa8376b49c84084f4a821579ad2990abb7f52b9
pascalc <pascal.chevrel@free.fr>
2012-12-18 15:41:46 +0100

    Reduce Npath/Cyclomatic complexity by moving some of extractData() code in other methods

--- src/xformat/Properties.php
+++ src/xformat/Properties.php
@@ -5,7 +5,7 @@
  * Licence: MPL 2/GPL 2.0/LGPL 2.1
  * Author: Pascal Chevrel, Mozilla <pascal@mozilla.com>, Mozilla
  * Date : 2012-12-07
- * version: 1.0
+ * version: 1.1beta
  * Description:
  * Class to extract key/value pairs from a java/js style .properties file
  * Supports comment extraction and multiline properties
@@ -38,11 +38,7 @@ class Properties
     {
         $source = file($this->source, 
                        FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        $source = array_map(
-            function ($elm) {
-                return trim($elm);
-            }, $source);
-
+        $source = $this->trimArrayElements($source);
         return $source;
     }
 
@@ -63,10 +59,7 @@ class Properties
             if (substr_count($line, '=') > substr_count($line, '\=')) {
 
                 $temp = explode('=', $line, 2);
-                $temp = array_map(
-                    function ($elm) {
-                        return trim($elm);
-                    }, $temp);
+                $temp = $this->trimArrayElements($temp);
 
                 if (count($temp) == 2) {
                     if (substr($temp[1], -1) == '"'
@@ -87,14 +80,7 @@ class Properties
         }
 
         /* Second pass, we associate comments to entities */
-
-        // count # of comments
-        $counter = 0;
-        foreach ($analysis as $v) {
-            if ($v[0] == 'comment') { 
-                $counter++; 
-            }
-        }
+        $counter = $this->getNumberLinesMatching('comment', $analysis);
 
         while ($counter > 0) {
 
@@ -114,36 +100,17 @@ class Properties
                 }
             }
 
-            $counter = 0;
-            foreach ($analysis as $k => $v) {
-                if ($v[0] == 'comment') {
-                    $counter++;
-                }
-                
-                if ($v[0] == 'erase') { 
-                    unset($analysis[$k]);
-                }
-            }
-
-            $analysis = array_values($analysis);
+			$counter  = $this->getNumberLinesMatching('comment', $analysis);
+            $analysis = $this->deleteField('erase', $analysis);
         }
 
         /* Third pass, we merge multiline strings */
 
         // We remove the backslashes at end of strings if they exist
-        foreach ($analysis as $line_nb => $line) {
-            if (substr($line[2], -1) == '\\') {
-                $analysis[$line_nb][2] = trim(substr($line[2], 0, -1));
-            }
-        }
+        $analysis = $this->stripBackslashes($analysis);
 
         // count # of multilines
-        $counter = 0;
-        foreach ($analysis as $v) {
-            if ($v[0] == 'multiline') {
-                $counter++;
-            }
-        }
+        $counter = $this->getNumberLinesMatching('multiline', $analysis);
 
         while ($counter > 0) {
             foreach ($analysis as $line_nb => $line) {
@@ -156,24 +123,13 @@ class Properties
                 }
             }
 
-            $counter = 0;
-            foreach ($analysis as $k => $v) {
-                if ($v[0] == 'multiline') {
-                    $counter++;
-                }
-                if ($v[0] == 'erase') {
-                    unset($analysis[$k]);
-                }
-            }
-
-            $analysis = array_values($analysis);
+            $counter  = $this->getNumberLinesMatching('multiline', $analysis);
+            $analysis = $this->deleteField('erase', $analysis);
         }
 
         /* Step 4, we clean up strings from escaped characters in properties */
-        foreach ($analysis as $k => $v) {
-            $analysis[$k][2] = str_replace('\=', '=', $v[2]);
-        }
-
+        $analysis = $this->unescapeProperties($analysis);
+        
         /* Step 5, we only have properties now, remove redondant field 0 */
         foreach ($analysis as $k => $v) {
             array_splice($analysis[$k], 0, 1);
@@ -182,14 +138,65 @@ class Properties
         return $analysis;
     }
 
+    private function unescapeProperties($analysis)
+    {
+        foreach ($analysis as $k => $v) {
+            $analysis[$k][2] = str_replace('\=', '=', $v[2]);
+        }
+        return $analysis;
+    }
+
+    public function stripBackslashes($tablo)
+    {
+        foreach ($tablo as $line_nb => $line) {
+            if (substr($line[2], -1) == '\\') {
+                $tablo[$line_nb][2] = trim(substr($line[2], 0, -1));
+            }
+        }
+        
+        return $tablo;
+    }
+
+    public static function trimArrayElements($tablo)
+    {
+        $tablo = array_map(
+        function ($elm) {
+            return trim($elm);
+        }, $tablo);
+
+        return $tablo;
+    }
+
+    private function getNumberLinesMatching($line_type, $analysis)
+    {
+        $counter = 0;
+        foreach ($analysis as $v) {
+            if ($v[0] == $line_type) { 
+                $counter++; 
+            }
+        }
+        return $counter;
+    }
+
+    private function deleteField($field, $analysis)
+    {
+        foreach ($analysis as $k => $v) {
+            if ($v[0] == $field) { 
+                unset($analysis[$k]);
+            }
+        }
+
+        return array_values($analysis);
+    }
+
     public function getProperties($file=false)
     {
         if ($file) {
             $this->setSourceFile($file);
         }
 
-        $source  = $this->extractData();
-        $data    = array();
+        $source = $this->extractData();
+        $data   = array();
 
         foreach ($source as $value) {
             $data[$value[0]] = $value[1];
